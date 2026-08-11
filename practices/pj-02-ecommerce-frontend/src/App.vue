c97de6f242eece084afbe1208b5009c0c01c085e
readant <3908492312@qq.com>
2026-08-11 21:57:12 +0800

    feat(ecommerce-frontend): F1.1 项目初始化脚手架搭建

---
+++ practices/pj-02-ecommerce-frontend/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body, #app {
+  height: 100%;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
+}
+</style>
